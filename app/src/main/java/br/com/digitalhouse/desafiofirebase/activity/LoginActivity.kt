29e8019d1b5b07ff00ce3b264d7e04fb37ea7d46
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2021-01-23 15:10:32 -0300

    login e cadastro

--- app/src/main/java/br/com/digitalhouse/desafiofirebase/activity/LoginActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafiofirebase/activity/LoginActivity.kt
@@ -1,12 +1,24 @@
 package br.com.digitalhouse.desafiofirebase.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.digitalhouse.desafiofirebase.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCadastro.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
