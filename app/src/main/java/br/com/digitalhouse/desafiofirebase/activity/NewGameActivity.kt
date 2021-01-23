1ab2592e9722c51428abed4b9240b68cf69604b1
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2021-01-23 17:42:27 -0300

    add new game activity

--- app/src/main/java/br/com/digitalhouse/desafiofirebase/activity/NewGameActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafiofirebase/activity/NewGameActivity.kt
@@ -3,10 +3,22 @@ package br.com.digitalhouse.desafiofirebase.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.digitalhouse.desafiofirebase.R
+import kotlinx.android.synthetic.main.activity_new_game.*
 
 class NewGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_game)
+
+        floatingActionButton3.setOnClickListener {
+            // TODO: pega imagem arquivo
+        }
+
+        btnSave.setOnClickListener {
+            inputName.text.toString()
+            inputAno.text.toString()
+            inputDescricao.text.toString()
+            // TODO: salva
+        }
     }
 }
